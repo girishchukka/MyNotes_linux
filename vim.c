0a516f4994403e9d22a5ce77a018b89a06ad7f6c
girish <girishchukka@gmail.com>
2018-02-23 00:39:14 +0530

    VIM file

---
+++ vim.c
@@ -0,0 +1,17 @@
+/* Basic commands */
+:w	- save
+:q	- quit
+:!	- overwrite
+:ls	- lists all opened files
+:pwd	- shows working directory
+
+To open a new file, use -
+:e <file_name>
+
+To see opened files, use -
+:ls
+
+To switch between opened files, use -
+:b<num>
+
+
